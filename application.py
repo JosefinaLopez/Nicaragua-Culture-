33d8ca7340a0622fb16939333df33db49f5ad365
Josefina López <87048053+JosefinaLopez@users.noreply.github.com>
2021-12-07 18:16:18 -0600

    cambios a application #4?

--- application.py
+++ application.py
@@ -5,22 +5,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-app = Flask(__name__)
+app=Flask(__name__)
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
     #condigurar la session
-    app.config["SESSION_PERMANENT"]= False
-    app.config["SESSION_TYPE"] = "filesystem"
-    Session(app)
-    #definiciones de base de datos
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+app.config["SESSION_PERMANENT"]= False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+    # base de datos
+engine=create_engine(os.getenv("DATABASE_URL"))
+db=scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.user_id: 
+        return render_template("index.html")
+    else:
+        return render_template("layout.html")
+  
 
 @app.route("/salir")
 def logout():
@@ -31,7 +35,7 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""                                       
-    if (request.method == "POST"):
+    if request.method == "POST":
         # declaracion de las variables
         username = request.form.get("username")
         password = request.form.get("password")
@@ -49,19 +53,23 @@ def register():
             return render_template("register.html")
 
        #encripta la contraseña
-        hash = generate_password_hash(password)
-        rows = db.execute("SELECT * FROM usuario WHERE username= :username", username)
+        password = generate_password_hash(password)
+        rows = db.execute("SELECT * FROM usuario WHERE username = :username",
+                          {"username": username}).fetchall()
 
-        if len(rows):
-            flash("usuario existente ")
+        if len(rows) != 0:
+            flash("usuario existente")
 
         # consula la base de datos para verificar el usuario
-        db.execute("INSERT INTO usuario (username, hash) VALUES (username= :username, hash= :hash)", username, hash)
+        variable=db.execute("INSERT INTO usuario (username, password, isadmin) VALUES ( :username, :password, False)", {"username": username,"password": password})
+        db.commit()
+        session["user_id"]= variable
+        flash("Usuario registrado")
         return redirect("/")
 
-        flash("Usuario existente")
+       
 
-    else:
+    else: 
         return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
@@ -76,8 +84,8 @@ def login():
 
         # usuario enviado
         if not request.form.get("username"):
-              flash("Proporcione un usuario")
-              return ("logint.html")
+            flash("Proporcione un usuario")
+            return ("logint.html")
 
         # contraseña enviada
         elif not request.form.get("password"):
@@ -86,7 +94,7 @@ def login():
 
         # base de datos del usuario
         rows = db.execute("SELECT * FROM usuario WHERE username = :username",
-                          username=request.form.get("username"))
+                        username=request.form.get("username"))
 
         # Error si el usuario existe o la contra es incorrecta
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
